45f093384df7364c4075768a72ecd946e2a8f44d
jeyaveeramuthumanoharim-droid <jeyaveeramuthumanoharim@gmail.com>
2026-03-30 12:06:36 +0530

    Add main function to print squares of numbers

---
+++ python/funcion.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # Read the integer n from input
+    n = int(input())
+    
+    # Loop from 0 to n-1
+    for i in range(n):
+        # Calculate the square of i and print it
+        # You can use i * i or i ** 2
+        print(i ** 2)
